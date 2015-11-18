8a379eec5de2ce6473d1db72ebbcc23cf6e32a1b
Geoff Longman <geoff@healthwave.co>
2015-11-18 15:47:58 -0500

    Add comment - Packer#default_package is not working correctly here. Fixed by override in hw_admin project.

--- core/app/models/spree/stock/packer.rb
+++ core/app/models/spree/stock/packer.rb
@@ -28,6 +28,13 @@ module Spree
             package.add line_item, on_hand, :on_hand if on_hand > 0
             package.add line_item, backordered, :backordered if backordered > 0
           else
+            # TODO - until Distributor is pulled into this fork from hw_admin
+            # there is no way to determine the correct course of action IF there was no stock
+            # item for the variant. This test passes here but does not work in the new Distributor aware
+            # world.
+            #
+            # To make it actually work for realz is to consult the Distributor's default inventory tracking flag.
+            # This is done in the Packer customizations in hw_admin :(
             package.add line_item, line_item.quantity, :on_hand unless Spree::Config.track_inventory_levels
           end
         end
